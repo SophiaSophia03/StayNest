f40f33719aa57c114764af3d5436898d5d2b75e1
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-03 15:58:53 -0500

    Created listing model

---
+++ models/listing.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title:{
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    set:(v) => v === ""? "https://images.unsplash.com/photo-1720884413532-59289875c3e1?q=80&w=435&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
+    default: "https://images.unsplash.com/photo-1720884413532-59289875c3e1?q=80&w=435&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  price:{
+    type: Number,
+  },
+  location:{
+    type: String,
+  },
+  country:{
+    type: String,
+  }
+});
+
+const Listing  = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
+
+
