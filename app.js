8a44bcb21f37e49434513059de71fd23c902b463
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-03 15:53:12 -0500

    Connecting to database

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+main().then((res) => {
+  console.log("Connection successful");
+})
+.catch(err => console.log(err));
+
+async function main(){
+  await mongoose.connect('mongodb://127.0.0.1:27017/StayNest');
+}
+
 app.get("/", (req,res) => {
   res.send("Hi, I am root");
 });
