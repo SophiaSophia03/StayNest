e3bdd8c4fe4fd052ac9b501577bf911b37ae9553
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-04 14:31:30 -0500

    Adding edit.ejs file

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
@@ -40,9 +42,21 @@ app.get("/listings/:id", async (req,res)=> {
   let {id} = req.params;
   const listing = await Listing.findById(id);
   res.render("./listings/show.ejs", {listing})
+});
+
+//Edit Route
+app.get("/listings/:id/edit",async(req,res) =>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", {listing});;
 })
 
-
+//Update Route
+app.put("/listings/:id", async(req,res) => {
+  let {id} = req.params;
+  const listing = await Listing.findByIdAndUpdate(id, req.body.listing, { new: true, runValidators: true });
+  res.redirect(`/listings/${id}`);
+})
 
 app.get("/", (req,res) => {
   res.send("Hi, I am root");
