7e3c8dd9fabf567e7010d81971318c82488c9462
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-08 15:55:51 -0500

    Added Validations for Schema

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 
 app.use(methodOverride("_method"));
@@ -26,6 +27,15 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/StayNest");
 }
 
+const validateListing = (req,res,next) => {
+  let {error}  = listingSchema.validate(req.body);
+    if(error){
+      let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400,errMsg);
+    }else{
+      next();
+    }
+}
 // Index route
 app.get("/listings", wrapAsync( async (req, res) => {
   const allListings = await Listing.find({});
@@ -39,11 +49,8 @@ app.get("/listings/new", (req, res) => {
 
 //Create Route
 app.post(
-  "/listings",
+  "/listings",validateListing,
   wrapAsync(async (req, res, next) => {
-    if(!req.body.listing){
-      throw new ExpressError(400, "Send valid data for listing");
-    }
       const newListing = new Listing(req.body.listing);
       await newListing.save();
       res.redirect("/listings");
@@ -65,7 +72,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, req.body.listing, {
     new: true,
