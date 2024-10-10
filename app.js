2309dea7625f4dc1c7878a0d3c623d0dd9eb7dd0
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-10 15:59:00 -0500

    Validate reviews from both client & server side

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 
@@ -30,6 +30,16 @@ async function main() {
 
 const validateListing = (req,res,next) => {
   let {error}  = listingSchema.validate(req.body);
+    if(error){
+      let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400,errMsg);
+    }else{
+      next();
+    }
+}
+
+const validateReview = (req,res,next) => {
+  let {error}  = reviewSchema.validate(req.body);
     if(error){
       let errMsg = error.details.map((el) => el.message).join(",");
       throw new ExpressError(400,errMsg);
@@ -91,7 +101,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 
 // Reviews //
 //Post Route
-app.post("/listings/:id/reviews",async (req,res) => {
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req,res) => {
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
   listing.reviews.push(newReview);
@@ -99,7 +109,7 @@ app.post("/listings/:id/reviews",async (req,res) => {
   await newReview.save();
   await listing.save();
   res.redirect(`/listings/${listing._id}`);
-})
+}));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
