33c9e313b5115cd3797dc3b61644ffb8a037150f
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-15 13:09:57 -0500

    Fixed Login, Signup and logout routes

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {listingSchema, reviewSchema} = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
 
 
 const validateListing = (req,res,next) => {
@@ -23,13 +24,13 @@ router.get("/", wrapAsync( async (req, res) => {
 }));
 
 //New Route
-router.get("/new", (req, res) => {
+router.get("/new",isLoggedIn, (req, res) => {
   res.render("./listings/new.ejs");
 });
 
 //Create Route
 router.post(
-  "/",validateListing,
+  "/",isLoggedIn,validateListing,
   wrapAsync(async (req, res, next) => {
       const newListing = new Listing(req.body.listing);
       await newListing.save();
@@ -50,14 +51,14 @@ router.get("/:id", wrapAsync(async (req, res) => {
 }));
 
 //Edit Route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("./listings/edit.ejs", { listing });
 }));
 
 //Update Route
-router.put("/:id",validateListing, wrapAsync(async (req, res) => {
+router.put("/:id",isLoggedIn,validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, req.body.listing, {
     new: true,
@@ -68,7 +69,7 @@ router.put("/:id",validateListing, wrapAsync(async (req, res) => {
 }));
 
 // Delete Route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id",isLoggedIn, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndDelete(id, req.body.listing);
   req.flash("success", "Listing Deleted!!");
--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ router.get("/signup", (req,res) => {
   res.render("./users/signup.ejs");
 })
 
-router.post("/signup", wrapAsync(async(req,res) => {
+router.post("/signup", wrapAsync(async(req,res, next) => {
   try{
     let {username,email,password} = req.body;
   let newUser = new User({
@@ -17,8 +17,13 @@ router.post("/signup", wrapAsync(async(req,res) => {
     username,
   });
   const registeredUser = await User.register(newUser,password );
-  req.flash("success", "Welcome!! New user registered successfuly");
-  res.redirect("/listings");
+  req.login(registeredUser, (err) => {
+    if(err){
+      next(err);
+    }
+    req.flash("success", "Welcome!! New user registered successfuly");
+    res.redirect("/listings");
+  })
   }catch(err){
     req.flash("error", err.message);
     res.redirect("/signup");
@@ -32,7 +37,17 @@ router.get("/login", (req,res) => {
 router.post("/login", passport.authenticate("local",{failureRedirect:"/login", failureFlash:true}), wrapAsync(async(req,res) => {
   req.flash("success","Welcome to StayNest, You logged in successfully");
   res.redirect("/listings");
-}))
+}));
+
+router.get("/logout", (req,res, next) => {
+  req.logout((err) => {
+    if(err){
+      next(err);
+    }
+    req.flash("success", "You are logged out!!");
+    res.redirect("/listings");
+  })
+})
 
 module.exports = router;
 
