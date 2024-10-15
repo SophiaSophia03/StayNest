ec80bf11617606a21a926c78cd79c4a58efde1d8
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-14 21:28:01 -0500

    Created User's model and implemented passport

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema({
+  email:{
+    type: String,
+    required: true,
+  }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User  = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+
