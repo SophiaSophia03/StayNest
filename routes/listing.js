ff96da8a19b9c73fc05cba00b86c91b38f27d97f
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-17 11:24:19 -0500

    SetUp Image upload functionality

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js")
+const upload = multer({storage})
 
 //Router.route implimentation
 router
@@ -12,9 +15,13 @@ router
     wrapAsync(listingController.index))
   .post(
     isLoggedIn,
-    validateListing,
+    // validateListing,
+    upload.single('listing[image]'),
     wrapAsync(listingController.createListing)
-  );
+  )
+  .post(upload.single('listing[image]'),(req,res) => {
+    res.send(req.file);
+  });
 
 //New Route
 router.get("/new",isLoggedIn,listingController.renderNewForm );
