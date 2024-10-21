d62320b3fa120cb368cc519653c27d6f9c076e47
SophiaSophia03 <sophiasophia1224@gmail.com>
2024-10-21 11:17:28 -0500

    Added geocoding and map marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,16 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  center: [77.2088, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9 // starting zoom
+  container: "map", // container ID
+  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
 });
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(coordinates)
+  .setPopup(new mapboxgl.Popup({
+    offset: 25,
+  })
+    .setHTML("<p>Exact Location will be provided after booking!</p>")
+    .setMaxWidth("300px"))
+  .addTo(map);
+
